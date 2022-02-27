7418c880cf54c8cd46c02355710610cfa1e9ccdb
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2022-02-26 21:25:52 -0500

    main section styling, start button groundwork laid

--- rpsls/src/App.js
+++ rpsls/src/App.js
@@ -2,21 +2,20 @@ import logo from './RPSLS.png';
 import './App.css';
 
 function App() {
-  
-  function toggleColorMode(event){ 
+
+  function toggleColorMode(event) {
     const self = event.target;
     const container = self.parentElement;
     const entireApp = container.parentElement;
-    if ( self.classList.contains('light-mode') ) { 
+    if (self.classList.contains('light-mode')) {
       self.classList.remove('light-mode');
       container.classList.remove('light-mode-container');
       entireApp.classList.remove('app-light-mode');
-    } else { 
+    } else {
       self.classList.add('light-mode');
       container.classList.add('light-mode-container');
       entireApp.classList.add('app-light-mode');
     }
-    
   }
 
   return (
@@ -25,21 +24,24 @@ function App() {
       <header className="App-header">
         <div className="color-mode-container">
           <div className="color-mode" onClick={toggleColorMode}></div></div>
-        <div className='content'>
-          <h1>
-            <span className="rock">Rock</span>,
-            <span className="paper">Paper</span>,
-            <span className="scissors">Scissors</span>, <br></br>
-            <span className="lizard">Lizard</span>,
-            <span className="spock">Spock</span>
-          </h1>
-          <img src={logo} className="App-logo" alt="logo" />
-          </div >
+        <div className='content-container'>
+          <div className='content'>
+            <h1>
+              <span className="rock">Rock</span>,
+              <span className="paper"> Paper</span>,
+              <span className="scissors"> Scissors</span>, <br></br>
+              <span className="lizard"> Lizard</span>,
+              <span className="spock"> Spock</span>
+            </h1>
+            <img src={logo} className="App-logo" alt="logo" />
+            <a href=''> S T A R T </a>
+          </div>
+        </div>
       </header>
     </div>
-    
+
   );
-  
+
 }
 
 
