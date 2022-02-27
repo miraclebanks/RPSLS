7d1c9b65f7942a4994461f6ee3e61029415b7666
FrostyyPaws <90424167+KenAKAFrosty@users.noreply.github.com>
2022-02-26 20:23:33 -0500

    laying groundwork for light & dark mode

--- rpsls/src/App.js
+++ rpsls/src/App.js
@@ -5,17 +5,22 @@ function App() {
 
   return (
     <div className="App">
+
       <header className="App-header">
-        <h1>
-          <span class="rock">Rock</span>,
-          <span class="paper">Paper</span>,
-          <span class="scissors">Scissors</span>, <br></br>
-          <span class="lizard">Lizard</span>,
-          <span class="spock">Spock</span>
-        </h1>
-        <img src={logo} className="App-logo" alt="logo" />
+        <div class="color-mode-container"><div class="color-mode"></div></div>
+        <div class='content'>
+          <h1>
+            <span class="rock">Rock</span>,
+            <span class="paper">Paper</span>,
+            <span class="scissors">Scissors</span>, <br></br>
+            <span class="lizard">Lizard</span>,
+            <span class="spock">Spock</span>
+          </h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          </div >
       </header>
     </div>
+    
   );
 }
 
